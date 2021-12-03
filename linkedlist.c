3fc08213238f950577ca95097082dc6220c75f05
lukkelele <encryptedluke@protonmail.com>
2021-12-03 08:38:20 +0100

    linkedlist.c update

--- linkedlist.c
+++ linkedlist.c
@@ -1,25 +1,61 @@
 #include <stdio.h>
 
 
-
 typedef struct node 
 {
-	int val;
+	int val, index;
 	struct node *prev, *next;
 } Node;
 
 
-Node head, tail, pn;
+Node *head, *tail;
 
 
-Node createNode(int val)
+Node newNode(int val)
 {
 	struct node newNode;
 	newNode.val = val;	
-
+	if (head == NULL) {
+		head = &newNode;
+		tail = &newNode;
+	}
+	
 	return newNode;
 }
 
+
+double countNodes(Node *head)
+{
+	double i;
+	Node n = *head;
+	while ((n.next != head)) {
+		i++;
+	}
+	return i;
+}
+
+
+void joinNode(Node *after, Node *joinee) 
+{
+	Node *buf = after->next;
+	after->next = joinee;
+	joinee->prev = after;
+	joinee->next = buf;
+	if (joinee->next == NULL) {
+		joinee->next = head;
+		tail = joinee;
+	}
+}
+
+
+void removeNode(Node *node) 
+{
+	node->prev = node->next;	// Remove the node and relink the chain}
+	if (node->next == NULL) {
+		node->prev->next = head;
+		node->prev = tail; 	// head <-> tail polling could be of use to make sure they always link
+	}
+}
 
 void showNode(Node node) 
 {
@@ -31,11 +67,22 @@ void showNode(Node node)
 int main(void)
 {
 
-	Node n = createNode(5);
-	Node n2 = createNode(3);
+	Node n = newNode(52);
+	Node n2 = newNode(3);
 
 	n2.prev = &n;
+	n2.next = head;
 	n.next = &n2;
+	n.prev = head;
+
+	Node n3 = newNode(12);
+	Node n4 = newNode(25);
+
+	n3.prev = &n2;
+	n3.next = &n;
+
+	joinNode(&n3, &n4);
+	printf("n3->next == %d\nn4->next == %d\n", n3.next->val, n4.next->val);
 
 	showNode(n);	
 	showNode(n2);
