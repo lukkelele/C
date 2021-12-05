8c7fd80e37502f61329cbb8b0a1b660ad36b3fc6
lukkelele <encryptedluke@protonmail.com>
2021-12-05 13:55:25 +0100

    Update

---
+++ keymap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void)
+{
+	
+	for (int k=0 ; k<10 ; k++) {
+		printf("OUT = %d\n", k);
+ 		for (int k=0 ; k<10 ; k++) {
+			 printf("IN = %d\n", k);
+			}
+	}
+
+	return 0;
+}
